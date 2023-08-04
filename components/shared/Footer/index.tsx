86a71522fff693a6f0f8f3e4520e307c195d6aaa
Sevak <sevak.sargsyan@sygnoos.com>
2023-08-05 01:11:07 +0400

    com1-29

--- components/shared/Footer/index.tsx
+++ components/shared/Footer/index.tsx
@@ -49,6 +49,9 @@ const Footer: FC<FooterProps> = ({ t }) => {
           </div>
         </div>
         <div className="d-flex flex-wrap col-12 col-md-7 justify-content-between">
+          <pre>
+            {JSON.stringify(linkColumns)}
+          </pre>
           {[1,2,3].map((x,i) => {console.log('LLLINK->',x); return <div key={i}>XXXXXXX</div>})}
           {/*{linkColumns?.map((column, i) => {*/}
           {/*  const { items, name } = column;*/}
